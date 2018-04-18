5fdf1d7c78118a555a08d6c2ae36c7978845c66d
Indelicato, Sven <sven.indelicato@accenture.com>
2018-04-18 13:59:20 +0200

    Added config-file

---
+++ src/config/config.js
@@ -0,0 +1,17 @@
+const config = {
+  // website url
+  url: 'http://localhost:3000/',
+
+  // Backend information
+  production: false,
+  backendBase: 'http://localhost:8080',
+
+  // Users API
+  backendApiUriUsers: '/api/users',
+
+  // Repository information
+  repository: 'https://github.com/svenssson93/ch.webapp.user.git',
+  branch: 'master',
+};
+
+export default config;
