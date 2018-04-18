c8f2fe2db2b6989442dbc3e782346d00c46bf183
Indelicato, Sven <sven.indelicato@accenture.com>
2018-04-18 13:59:44 +0200

    Updated App-Component to show Users-Table.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
+import config from './config/config';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      users: [],
+    }
+  }
+
+  componentWillMount() {
+    console.log('Fetching data from backend...');
+    fetch(config.backendBase + config.backendApiUriUsers).then((resp) => {
+      resp.json().then((json) => {
+        console.log(json);
+        this.setState({
+          users: json['_embedded']['users'],
+        })
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,12 +35,36 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <MuiThemeProvider>
+          <Paper>
+            <Table className="tblUsers">
+              <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+                <TableRow>
+                  <TableHeaderColumn>Id</TableHeaderColumn>
+                  <TableHeaderColumn>Name</TableHeaderColumn>
+                  <TableHeaderColumn>E-Mail</TableHeaderColumn>
+                  <TableHeaderColumn>Nickname</TableHeaderColumn>
+                </TableRow>
+              </TableHeader>
+              <TableBody id="tblUser" displayRowCheckbox={false}>
+                {this.state.users.map((user) => {
+                  return(
+                    <TableRow key={user['id']}>
+                      <TableRowColumn>{user['id']}</TableRowColumn>
+                      <TableRowColumn>{user['name']}</TableRowColumn>
+                      <TableRowColumn>{user['email']}</TableRowColumn>
+                      <TableRowColumn>{user['nickname']}</TableRowColumn>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </Paper>
+        </MuiThemeProvider>
       </div>
     );
   }
+
 }
 
 export default App;
